8c74dd2261ff8ef194bd6696ce86c052832420c2
Feng Liu <feng.liu@dtone.com>
2025-01-25 10:58:37 +0800

    applicant handler, render

---
+++ handler/render.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+const ContentTypeJSON = "application/json; charset=utf-8"
+
+func (h *Handler) Render(statusCode int, w http.ResponseWriter, v interface{}) {
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
+	enc.SetEscapeHTML(true)
+	if err := enc.Encode(v); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", ContentTypeJSON)
+	w.WriteHeader(statusCode)
+	_, err := w.Write(buf.Bytes())
+	if err != nil {
+		h.logger.Error("Writing HTTP response failed", zap.Error(err), zap.Any("data", v))
+	}
+}
--- handler/router.go
+++ handler/router.go
@@ -2,20 +2,25 @@ package handler
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
 
 	applcux "github.com/lftzzzzfeng/fasms/usecases/applicant"
 )
 
 type HandlerConfig struct {
+	Logger  *zap.Logger
 	ApplcUx *applcux.Applicant
 }
 
 type Handler struct {
+	logger  *zap.Logger
 	applcUx *applcux.Applicant
 }
 
 func New(hdlConf *HandlerConfig) *Handler {
 	return &Handler{
+		logger:  hdlConf.Logger,
 		applcUx: hdlConf.ApplcUx,
 	}
 }
@@ -23,7 +28,9 @@ func New(hdlConf *HandlerConfig) *Handler {
 func (h *Handler) Router() (chi.Router, error) {
 	router := chi.NewRouter()
 
-	h.applcUx.CreateApplicant()
+	router.Use(middleware.AllowContentType("application/json"))
+
+	router.Get("/api/applicants", h.applicantHandler)
 
 	return router, nil
 }
